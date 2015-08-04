cf9b83080adb447a3a093c5639d2ef01506a108b
Jian JIAO <mynetuser@outlook.com>
2015-08-04 10:55:01 +0800

    Refactor SDCard
    - It works well.
    * Shoule add DSPI device open detect.

--- Project_Headers/SDCard.h
+++ Project_Headers/SDCard.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * SDCard.h
  *
  *  Created on: Jul 27, 2015
@@ -8,220 +8,21 @@
 #pragma once
 
 
-#define SDCARD_DSPI_PUSHR_CONT                          (1)
-#define SDCARD_DSPI_PUSHR_PCS                           (DSPI_PUSHR_PCS_0)
-#define SDCARD_DSPI_CTAR_DBR                            (0b0)
-#define SDCARD_DSPI_CTAR_CPOL                           (0b0)
-#define SDCARD_DSPI_CTAR_CPHA                           (0)
-#define SDCARD_DSPI_CTAR_LSBFE                          (0b0)
-#define SDCARD_DSPI_CTAR_PCSSCK                         (0b00)
-#define SDCARD_DSPI_CTAR_PASC                           (0b00)
-#define SDCARD_DSPI_CTAR_PDT                            (0b10)
-#define SDCARD_DSPI_CTAR_PBR                            (0b00)
-#define SDCARD_DSPI_CTAR_CSSCK                          (0b0111)
-#define SDCARD_DSPI_CTAR_ASC                            (0b0111)
-#define SDCARD_DSPI_CTAR_DT                             (0b0010)
-#define SDCARD_DSPI_CTAR_BR                             (0b0110)
 #define SDCARD_SECTOR_SIZE                              (512)
+#define SDCARD_RES_OK                                   (0)
 #define SDCARD_ERR_WRITE_BLOCK_FAILED                   (1)
 #define SDCARD_OVERTIME_DEFAULT                         (1000)
 #define SDCARD_OVERTIME_READ                            (100)
 #define SDCARD_OVERTIME_WRITE                           (250)
 #define SDCARD_OVERTIME_ERASE                           (250)
-#define SDCARD_CMD0                                     (0)
-#define SDCARD_CMD1                                     (1)
-#define SDCARD_CMD8                                     (8)
-#define SDCARD_CMD9                                     (9)
-#define SDCARD_CMD12                                    (12)
-#define SDCARD_CMD16                                    (16)
-#define SDCARD_CMD17                                    (17)
-#define SDCARD_CMD18                                    (18)
-#define SDCARD_CMD24                                    (24)
-#define SDCARD_CMD25                                    (25)
-#define SDCARD_CMD55                                    (55)
-#define SDCARD_CMD59                                    (59)
-#define SDCARD_CMD58                                    (58)
-#define SDCARD_ACMD41                                   (41)
 #define SDCARD_VERSION_1                                (10)
 #define SDCARD_VERSION_2_STANDARD_CAPACITY              (21)
 #define SDCARD_VERSION_2_HIGH_CAPACITY                  (22)
 #define SDCARD_VERSION_UNUSABLE                         (0)
 
-typedef union
-{
-    uint8_t R[1];
-    struct
-    {
-        uint8_t                 : 1;
-        uint8_t Parameter_Err   : 1;
-        uint8_t Address_Error   : 1;
-        uint8_t Erase_Seq_Error : 1;
-        uint8_t Com_CRC_Error   : 1;
-        uint8_t Illegal_Command : 1;
-        uint8_t Erase_Reset     : 1;
-        uint8_t In_Idle_State   : 1;
-    } B;
-} R1;
 
-typedef union
-{
-    uint8_t R[2];
-    struct
-    {
-        R1 R1;
-        uint8_t Out_of_Range_CSD_Overwrite              : 1;
-        uint8_t Erase_Param                             : 1;
-        uint8_t WP_Violation                            : 1;
-        uint8_t Card_ECC_Failed                         : 1;
-        uint8_t CC_Error                                : 1;
-        uint8_t Error                                   : 1;
-        uint8_t WP_Erase_Skip_Lock_Unlock_Cmd_Failed    : 1;
-        uint8_t Card_is_Locked                          : 1;
-    } B;
-} R2;
-
-typedef union
-{
-    uint8_t R[4];
-    struct
-    {
-        uint8_t Card_power_up_status_bit    : 1;
-        uint8_t CCS                         : 1;
-        uint8_t                             : 6;
-        uint8_t V3P5_3P6                    : 1;
-        uint8_t V3P4_3P5                    : 1;
-        uint8_t V3P3_3P4                    : 1;
-        uint8_t V3P2_3P3                    : 1;
-        uint8_t V3P1_3P2                    : 1;
-        uint8_t V3P0_3P1                    : 1;
-        uint8_t V2P9_3P0                    : 1;
-        uint8_t V2P8_2P9                    : 1;
-        uint8_t V2P7_2P8                    : 1;
-        uint8_t                             : 7;
-        uint8_t                             : 8;
-    } B;
-} OCR;
-
-typedef union
-{
-    uint8_t R[16];
-    union
-    {
-        struct
-        {
-            uint32_t CSD_STRUCTURE          : 2;
-            uint32_t                        : 6;
-            uint32_t TAAC                   : 8;
-            uint32_t NSAC                   : 8;
-            uint32_t TRAN_SPEED             : 8;
-            uint32_t CCC                    : 12;
-            uint32_t READ_BL_LEN            : 4;
-            uint32_t READ_BL_PARTIAL        : 1;
-            uint32_t WRITE_BLK_MISALIGN     : 1;
-            uint32_t READ_BLK_MISALIGN      : 1;
-            uint32_t DSR_IMP                : 1;
-            uint32_t                        : 2;
-            uint32_t C_SIZE_H               : 10;
-            uint32_t C_SIZE_L               : 2;
-            uint32_t VDD_R_CURR_MIN         : 3;
-            uint32_t VDD_R_CURR_MAX         : 3;
-            uint32_t VDD_W_CURR_MIN         : 3;
-            uint32_t VDD_W_CURR_MAX         : 3;
-            uint32_t C_SIZE_MULT            : 3;
-            uint32_t ERASE_BLK_EN           : 1;
-            uint32_t SECTOR_SIZE            : 7;
-            uint32_t WP_GRP_SIZE            : 7;
-            uint32_t WP_GRP_ENABLE          : 1;
-            uint32_t                        : 2;
-            uint32_t R2W_FACTOR             : 3;
-            uint32_t WRITE_BL_LEN           : 4;
-            uint32_t WRITE_BL_PARTIAL       : 1;
-            uint32_t                        : 5;
-            uint32_t FILE_FORMAT_GRP        : 1;
-            uint32_t COPY                   : 1;
-            uint32_t PERM_WRITE_PROTECT     : 1;
-            uint32_t TMP_WRITE_PROTECT      : 1;
-            uint32_t FILE_FORMAT            : 2;
-            uint32_t                        : 2;
-            uint32_t CRC                    : 7;
-            uint32_t                        : 1;
-        } CSD_V1_0;
-        struct
-        {
-            uint32_t CSD_STRUCTURE          : 2;
-            uint32_t                        : 6;
-            uint32_t TAAC                   : 8;
-            uint32_t NSAC                   : 8;
-            uint32_t TRAN_SPEED             : 8;
-            uint32_t CCC                    : 12;
-            uint32_t READ_BL_LEN            : 4;
-            uint32_t READ_BL_PARTIAL        : 1;
-            uint32_t WRITE_BLK_MISALIGN     : 1;
-            uint32_t READ_BLK_MISALIGN      : 1;
-            uint32_t DSR_IMP                : 1;
-            uint32_t                        : 6;
-            uint32_t C_SIZE_H               : 6;
-            uint32_t C_SIZE_L               : 16;
-            uint32_t                        : 1;
-            uint32_t ERASE_BLK_EN           : 1;
-            uint32_t SECTOR_SIZE            : 7;
-            uint32_t WP_GRP_SIZE            : 7;
-            uint32_t WP_GRP_ENABLE          : 1;
-            uint32_t                        : 2;
-            uint32_t R2W_FACTOR             : 3;
-            uint32_t WRITE_BL_LEN           : 4;
-            uint32_t WRITE_BL_PARTIAL       : 1;
-            uint32_t                        : 5;
-            uint32_t FILE_FORMAT_GRP        : 1;
-            uint32_t COPY                   : 1;
-            uint32_t PERM_WRITE_PROTECT     : 1;
-            uint32_t TMP_WRITE_PROTECT      : 1;
-            uint32_t FILE_FORMAT            : 2;
-            uint32_t                        : 2;
-            uint32_t CRC                    : 7;
-            uint32_t                        : 1;
-        } CSD_V2_0;
-    } B;
-} CSD;
-
-typedef union
-{
-    uint8_t R[5];
-    struct
-    {
-        R1 R1;
-        OCR OCR;
-    } B;
-} R3;
-
-typedef struct
-{
-    struct DSPI_Device_Data *DSPI_dev;
-    int version;
-    uint32_t sector_amount;
-    CSD CSD;
-} SDCard_Dev_Data;
-
-typedef union
-{
-    uint8_t R[5];
-    struct
-    {
-        R1 R1;
-        uint8_t command_version     : 4;
-        uint8_t                     : 4;
-        uint8_t                     : 8;
-        uint8_t                     : 4;
-        uint8_t voltage_accepted    : 4;
-        uint8_t check_pattern       : 8;
-    } B;
-} R7;
-
-
-extern      int                 Set_DSPI_Device(struct DSPI_Device_Data *dspi);
-extern      int                 Test_SDCard(void);
-extern      int                 Reset_SDCard(void);
-extern      int                 Test_SDCard_Read_Block(uint32_t sector, uint8_t btffer[]);
-extern      int                 Test_SDCard_Read_Mult_Block(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt);
-extern      int                 Test_SDCard_Write_Block(uint32_t sector, uint8_t buffer[]);
-extern      int                 Test_SDCard_Write_Mult_Blocks(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt);
+extern      int                 Init_SDCard(void);
+extern      int                 SDCard_Read_Single_Block(uint32_t sector, uint8_t btffer[]);
+extern      int                 SDCard_Read_Mult_Block(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt);
+extern      int                 SDCard_Write_Single_Block(uint32_t sector, uint8_t buffer[]);
+extern      int                 SDCard_Write_Mult_Blocks(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt);
--- Sources/SDCard.c
+++ Sources/SDCard.c
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * SDCard.c
  *
  *  Created on: Jul 27, 2015
@@ -10,29 +10,233 @@
 #include        "includes.h"
 
 
+#define SDCARD_DSPI_PUSHR_CONT                          (1)
+#define SDCARD_DSPI_PUSHR_PCS                           (DSPI_PUSHR_PCS_0)
+#define SDCARD_DSPI_CTAR_DBR                            (0b0)
+#define SDCARD_DSPI_CTAR_CPOL                           (0b0)
+#define SDCARD_DSPI_CTAR_CPHA                           (0)
+#define SDCARD_DSPI_CTAR_LSBFE                          (0b0)
+#define SDCARD_DSPI_CTAR_PCSSCK                         (0b00)
+#define SDCARD_DSPI_CTAR_PASC                           (0b00)
+#define SDCARD_DSPI_CTAR_PDT                            (0b10)
+#define SDCARD_DSPI_CTAR_PBR                            (0b00)
+#define SDCARD_DSPI_CTAR_CSSCK                          (0b0111)
+#define SDCARD_DSPI_CTAR_ASC                            (0b0111)
+#define SDCARD_DSPI_CTAR_DT                             (0b0010)
+#define SDCARD_DSPI_CTAR_BR                             (0b0110)
+#define SDCARD_CMD0                                     (0)
+#define SDCARD_CMD1                                     (1)
+#define SDCARD_CMD8                                     (8)
+#define SDCARD_CMD9                                     (9)
+#define SDCARD_CMD12                                    (12)
+#define SDCARD_CMD16                                    (16)
+#define SDCARD_CMD17                                    (17)
+#define SDCARD_CMD18                                    (18)
+#define SDCARD_CMD24                                    (24)
+#define SDCARD_CMD25                                    (25)
+#define SDCARD_CMD55                                    (55)
+#define SDCARD_CMD59                                    (59)
+#define SDCARD_CMD58                                    (58)
+#define SDCARD_ACMD41                                   (41)
+
+
+typedef union
+{
+    uint8_t R[1];
+    struct
+    {
+        uint8_t                 : 1;
+        uint8_t Parameter_Err   : 1;
+        uint8_t Address_Error   : 1;
+        uint8_t Erase_Seq_Error : 1;
+        uint8_t Com_CRC_Error   : 1;
+        uint8_t Illegal_Command : 1;
+        uint8_t Erase_Reset     : 1;
+        uint8_t In_Idle_State   : 1;
+    } B;
+} R1;
+
+typedef union
+{
+    uint8_t R[2];
+    struct
+    {
+        R1 R1;
+        uint8_t Out_of_Range_CSD_Overwrite              : 1;
+        uint8_t Erase_Param                             : 1;
+        uint8_t WP_Violation                            : 1;
+        uint8_t Card_ECC_Failed                         : 1;
+        uint8_t CC_Error                                : 1;
+        uint8_t Error                                   : 1;
+        uint8_t WP_Erase_Skip_Lock_Unlock_Cmd_Failed    : 1;
+        uint8_t Card_is_Locked                          : 1;
+    } B;
+} R2;
+
+typedef union
+{
+    uint8_t R[4];
+    struct
+    {
+        uint8_t Card_power_up_status_bit    : 1;
+        uint8_t CCS                         : 1;
+        uint8_t                             : 6;
+        uint8_t V3P5_3P6                    : 1;
+        uint8_t V3P4_3P5                    : 1;
+        uint8_t V3P3_3P4                    : 1;
+        uint8_t V3P2_3P3                    : 1;
+        uint8_t V3P1_3P2                    : 1;
+        uint8_t V3P0_3P1                    : 1;
+        uint8_t V2P9_3P0                    : 1;
+        uint8_t V2P8_2P9                    : 1;
+        uint8_t V2P7_2P8                    : 1;
+        uint8_t                             : 7;
+        uint8_t                             : 8;
+    } B;
+} OCR;
+
+typedef union
+{
+    uint8_t R[16];
+    union
+    {
+        struct
+        {
+            uint32_t CSD_STRUCTURE          : 2;
+            uint32_t                        : 6;
+            uint32_t TAAC                   : 8;
+            uint32_t NSAC                   : 8;
+            uint32_t TRAN_SPEED             : 8;
+            uint32_t CCC                    : 12;
+            uint32_t READ_BL_LEN            : 4;
+            uint32_t READ_BL_PARTIAL        : 1;
+            uint32_t WRITE_BLK_MISALIGN     : 1;
+            uint32_t READ_BLK_MISALIGN      : 1;
+            uint32_t DSR_IMP                : 1;
+            uint32_t                        : 2;
+            uint32_t C_SIZE_H               : 10;
+            uint32_t C_SIZE_L               : 2;
+            uint32_t VDD_R_CURR_MIN         : 3;
+            uint32_t VDD_R_CURR_MAX         : 3;
+            uint32_t VDD_W_CURR_MIN         : 3;
+            uint32_t VDD_W_CURR_MAX         : 3;
+            uint32_t C_SIZE_MULT            : 3;
+            uint32_t ERASE_BLK_EN           : 1;
+            uint32_t SECTOR_SIZE            : 7;
+            uint32_t WP_GRP_SIZE            : 7;
+            uint32_t WP_GRP_ENABLE          : 1;
+            uint32_t                        : 2;
+            uint32_t R2W_FACTOR             : 3;
+            uint32_t WRITE_BL_LEN           : 4;
+            uint32_t WRITE_BL_PARTIAL       : 1;
+            uint32_t                        : 5;
+            uint32_t FILE_FORMAT_GRP        : 1;
+            uint32_t COPY                   : 1;
+            uint32_t PERM_WRITE_PROTECT     : 1;
+            uint32_t TMP_WRITE_PROTECT      : 1;
+            uint32_t FILE_FORMAT            : 2;
+            uint32_t                        : 2;
+            uint32_t CRC                    : 7;
+            uint32_t                        : 1;
+        } CSD_V1_0;
+        struct
+        {
+            uint32_t CSD_STRUCTURE          : 2;
+            uint32_t                        : 6;
+            uint32_t TAAC                   : 8;
+            uint32_t NSAC                   : 8;
+            uint32_t TRAN_SPEED             : 8;
+            uint32_t CCC                    : 12;
+            uint32_t READ_BL_LEN            : 4;
+            uint32_t READ_BL_PARTIAL        : 1;
+            uint32_t WRITE_BLK_MISALIGN     : 1;
+            uint32_t READ_BLK_MISALIGN      : 1;
+            uint32_t DSR_IMP                : 1;
+            uint32_t                        : 6;
+            uint32_t C_SIZE_H               : 6;
+            uint32_t C_SIZE_L               : 16;
+            uint32_t                        : 1;
+            uint32_t ERASE_BLK_EN           : 1;
+            uint32_t SECTOR_SIZE            : 7;
+            uint32_t WP_GRP_SIZE            : 7;
+            uint32_t WP_GRP_ENABLE          : 1;
+            uint32_t                        : 2;
+            uint32_t R2W_FACTOR             : 3;
+            uint32_t WRITE_BL_LEN           : 4;
+            uint32_t WRITE_BL_PARTIAL       : 1;
+            uint32_t                        : 5;
+            uint32_t FILE_FORMAT_GRP        : 1;
+            uint32_t COPY                   : 1;
+            uint32_t PERM_WRITE_PROTECT     : 1;
+            uint32_t TMP_WRITE_PROTECT      : 1;
+            uint32_t FILE_FORMAT            : 2;
+            uint32_t                        : 2;
+            uint32_t CRC                    : 7;
+            uint32_t                        : 1;
+        } CSD_V2_0;
+    } B;
+} CSD;
+
+typedef union
+{
+    uint8_t R[5];
+    struct
+    {
+        R1 R1;
+        OCR OCR;
+    } B;
+} R3;
+
+typedef struct
+{
+    struct DSPI_Device_Data *DSPI_dev;
+    int version;
+    uint32_t sector_amount;
+    CSD CSD;
+} SDCard_Dev_Data;
+
+typedef union
+{
+    uint8_t R[5];
+    struct
+    {
+        R1 R1;
+        uint8_t command_version     : 4;
+        uint8_t                     : 4;
+        uint8_t                     : 8;
+        uint8_t                     : 4;
+        uint8_t voltage_accepted    : 4;
+        uint8_t check_pattern       : 8;
+    } B;
+} R7;
+
+
 static      uint8_t                         Test_SD_Buff[4][SDCARD_SECTOR_SIZE];
 static      SDCard_Dev_Data                 SDCard_Dev_Data_1;
 
+
+static      int                             Set_DSPI_Device(struct DSPI_Device_Data *dspi);
+static      int                             Reset_SDCard(void);
 static      int                             Rev_8_Bytes(uint8_t data[]);
 static      int                             Send_8_Bytes(uint8_t data[]);
 static      int                             Send_8_Clocks_withoout_CS(void);
 static      int                             Send_80_Clocks_without_CS(void);
 static      void                            Send_Byte(uint8_t data);
 static      uint8_t                         Rev_Byte(void);
-static      int                             Test_Get_CSD(void);
-static      int                             Test_SDCard_Send_Cmd_Ex2(int cmd, uint32_t var, uint8_t crc, uint8_t *rev, int len);
+static      int                             Get_CSD(void);
+static      int                             Test_SDCard_Send_Cmd(int cmd, uint32_t var, uint8_t crc, uint8_t *rev, int len);
 static      uint8_t                         Cal_CRC7(uint8_t *data, int cnt);
 static      int                             Get_SDCard_Size(void);
 
 
-int Set_DSPI_Device(struct DSPI_Device_Data *dspi)
+static int Set_DSPI_Device(struct DSPI_Device_Data *dspi)
 {
     SDCard_Dev_Data_1.DSPI_dev = dspi;
     return 0;
 }
 
 
-int Test_SDCard(void)
+int Init_SDCard(void)
 {
     int rev = 0;
     uint32_t size = 0;
@@ -42,42 +246,12 @@ int Test_SDCard(void)
     Set_DSPI_CTAR(SDCard_Dev_Data_1.DSPI_dev, SDCARD_DSPI_CTAR_DBR, SDCARD_DSPI_CTAR_CPOL, SDCARD_DSPI_CTAR_CPHA, SDCARD_DSPI_CTAR_LSBFE, SDCARD_DSPI_CTAR_PCSSCK, SDCARD_DSPI_CTAR_PASC, SDCARD_DSPI_CTAR_PDT, SDCARD_DSPI_CTAR_PBR, SDCARD_DSPI_CTAR_CSSCK, SDCARD_DSPI_CTAR_ASC, SDCARD_DSPI_CTAR_DT, SDCARD_DSPI_CTAR_BR);
     Set_DSPI_PUSHR(SDCard_Dev_Data_1.DSPI_dev, SDCARD_DSPI_PUSHR_CONT, SDCARD_DSPI_PUSHR_PCS);
     Reset_SDCard();
-    Test_Get_CSD();
+    Get_CSD();
     Get_SDCard_Size();
-    
-    do
-    {
-        rev = Test_SDCard_Read_Block(0, Test_SD_Buff[0]);
-    }
-    while(rev);
-    
-    do
-    {
-        rev = Test_SDCard_Read_Block(1, Test_SD_Buff[1]);
-    }
-    while(rev);
-    
-    do
-    {
-        rev = Test_SDCard_Write_Mult_Blocks(4, (uint8_t (*)[SDCARD_SECTOR_SIZE])(Test_SD_Buff[0]), 2);
-    }
-    while(rev);
-    
-    do
-    {
-        rev = Test_SDCard_Read_Block(4, Test_SD_Buff[2]);
-    }
-    while(rev);
-    
-    do
-    {
-        rev = Test_SDCard_Read_Block(5, Test_SD_Buff[3]);
-    }
-    while(1);
 }
 
 
-static int Test_SDCard_Send_Cmd_Ex2(int cmd, uint32_t var, uint8_t crc, uint8_t *rev, int len)
+static int Test_SDCard_Send_Cmd(int cmd, uint32_t var, uint8_t crc, uint8_t *rev, int len)
 {
     uint8_t send[6], res = 0;
     
@@ -100,7 +274,7 @@ static int Test_SDCard_Send_Cmd_Ex2(int cmd, uint32_t var, uint8_t crc, uint8_t 
 }
 
 
-int Reset_SDCard(void)
+static int Reset_SDCard(void)
 {
     R7 r7;
     R1 r1;
@@ -112,13 +286,13 @@ int Reset_SDCard(void)
     Send_80_Clocks_without_CS();
     do
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD0, 0, 0x95, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD0, 0, 0x95, r1.R, sizeof(r1));
     }
     while (0x01 != r1.R[0]);
 
     while(1)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD8, 0x000001AA, crc, r7.R, sizeof(r7));
+        Test_SDCard_Send_Cmd(SDCARD_CMD8, 0x000001AA, crc, r7.R, sizeof(r7));
         if (1 == r7.B.R1.B.Illegal_Command)
         {
             SDCard_Dev_Data_1.version = SDCARD_VERSION_1;
@@ -140,7 +314,7 @@ int Reset_SDCard(void)
     }
     while(1)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD58, 0x00000000, 0xFF, r3.R, sizeof(r3));
+        Test_SDCard_Send_Cmd(SDCARD_CMD58, 0x00000000, 0xFF, r3.R, sizeof(r3));
         if (0x01 != r3.B.R1.R[0])
         {
             continue;
@@ -158,12 +332,12 @@ int Reset_SDCard(void)
     
     while(1)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD55, 0x00000000, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD55, 0x00000000, 0xFF, r1.R, sizeof(r1));
         if (0x01 != r1.R[0])
         {
             continue;
         }
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_ACMD41, 0x40000000, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_ACMD41, 0x40000000, 0xFF, r1.R, sizeof(r1));
         if (0x00 == r1.R[0])
         {
             break;
@@ -172,7 +346,7 @@ int Reset_SDCard(void)
     
     while(1)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD58, 0x00000000, 0xFF, r3.R, sizeof(r3));
+        Test_SDCard_Send_Cmd(SDCARD_CMD58, 0x00000000, 0xFF, r3.R, sizeof(r3));
         if (0x00 != r3.B.R1.R[0])
         {
             continue;
@@ -195,7 +369,7 @@ int Reset_SDCard(void)
     }
     while(1)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD59, 0, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD59, 0, 0xFF, r1.R, sizeof(r1));
         if (0x00 == r1.R[0])
         {
             break;
@@ -203,7 +377,7 @@ int Reset_SDCard(void)
     }
     while(1)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD16, 512, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD16, 512, 0xFF, r1.R, sizeof(r1));
         if (0x00 == r1.R[0])
         {
             break;
@@ -213,7 +387,7 @@ int Reset_SDCard(void)
 }
 
 
-int Test_SDCard_Read_Block(uint32_t sector, uint8_t buffer[])
+int SDCard_Read_Single_Block(uint32_t sector, uint8_t buffer[])
 {
     R1 r1;
     int i = 0;
@@ -221,11 +395,11 @@ int Test_SDCard_Read_Block(uint32_t sector, uint8_t buffer[])
     
     if (SDCARD_VERSION_2_STANDARD_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD17, sector<<9, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD17, sector<<9, 0xFF, r1.R, sizeof(r1));
     }
     else if (SDCARD_VERSION_2_HIGH_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD17, sector, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD17, sector, 0xFF, r1.R, sizeof(r1));
     }
     else
     {
@@ -256,7 +430,7 @@ int Test_SDCard_Read_Block(uint32_t sector, uint8_t buffer[])
 }
 
 
-int Test_SDCard_Read_Mult_Block(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt)
+int SDCard_Read_Mult_Block(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt)
 {
     R1 r1;
     int i = 0, j = 0;
@@ -264,11 +438,11 @@ int Test_SDCard_Read_Mult_Block(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_
     
     if (SDCARD_VERSION_2_STANDARD_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD18, sector<<9, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD18, sector<<9, 0xFF, r1.R, sizeof(r1));
     }
     else if(SDCARD_VERSION_2_HIGH_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD18, sector, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD18, sector, 0xFF, r1.R, sizeof(r1));
     }
     else
     {
@@ -299,13 +473,13 @@ int Test_SDCard_Read_Mult_Block(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_
         }
         DSPI_SYNC_Send_and_Receive_Data(SDCard_Dev_Data_1.DSPI_dev, send, NULL, 2);
     }
-    Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD12, 0, 0xFF, r1.R, sizeof(r1));
+    Test_SDCard_Send_Cmd(SDCARD_CMD12, 0, 0xFF, r1.R, sizeof(r1));
     Send_8_Clocks_withoout_CS();
     return 0;
 }
 
 
-int Test_SDCard_Write_Block(uint32_t sector, uint8_t buffer[])
+int SDCard_Write_Single_Block(uint32_t sector, uint8_t buffer[])
 {
     R1 r1;
     int i = 0;
@@ -314,11 +488,11 @@ int Test_SDCard_Write_Block(uint32_t sector, uint8_t buffer[])
     
     if (SDCARD_VERSION_2_STANDARD_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD24, sector<<9, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD24, sector<<9, 0xFF, r1.R, sizeof(r1));
     }
     else if (SDCARD_VERSION_2_HIGH_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD24, sector, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD24, sector, 0xFF, r1.R, sizeof(r1));
     }
     else
     {
@@ -355,7 +529,7 @@ int Test_SDCard_Write_Block(uint32_t sector, uint8_t buffer[])
 }
 
 
-int Test_SDCard_Write_Mult_Blocks(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt)
+int SDCard_Write_Mult_Blocks(uint32_t sector, uint8_t buffer[][SDCARD_SECTOR_SIZE], int block_cnt)
 {
     R1 r1;
     int i = 0, j = 0;
@@ -364,11 +538,11 @@ int Test_SDCard_Write_Mult_Blocks(uint32_t sector, uint8_t buffer[][SDCARD_SECTO
     
     if (SDCARD_VERSION_2_STANDARD_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD25, sector<<9, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD25, sector<<9, 0xFF, r1.R, sizeof(r1));
     }
     else if (SDCARD_VERSION_2_HIGH_CAPACITY == SDCard_Dev_Data_1.version)
     {
-        Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD25, sector, 0xFF, r1.R, sizeof(r1));
+        Test_SDCard_Send_Cmd(SDCARD_CMD25, sector, 0xFF, r1.R, sizeof(r1));
     }
     else
     {
@@ -473,12 +647,12 @@ static uint8_t Rev_Byte(void)
 }
 
 
-static int Test_Get_CSD(void)
+static int Get_CSD(void)
 {
     R1 r1;
     
     Set_DSPI_PUSHR(SDCard_Dev_Data_1.DSPI_dev, SDCARD_DSPI_PUSHR_CONT, SDCARD_DSPI_PUSHR_PCS);
-    Test_SDCard_Send_Cmd_Ex2(SDCARD_CMD9, 0x00000000, 0xFF, r1.R, sizeof(r1));
+    Test_SDCard_Send_Cmd(SDCARD_CMD9, 0x00000000, 0xFF, r1.R, sizeof(r1));
     if (0x00 != r1.R[0])
     {
         return (int)r1.R[0];
