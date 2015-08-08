f9f98166b64c5b23b4d37b20dcec706f204d8230
Jian JIAO <mynetuser@outlook.com>
2015-08-08 19:53:01 +0800

    Change OLED Internal RAM Access Method
    - Send data to OLED internal ram once.
    - It works well.

--- Sources/oled.c
+++ Sources/oled.c
@@ -39,7 +39,7 @@ OLED_RES Init_OLED(void)
         Delay_ms(50);
     }
     {
-        uint8_t cmds[] = {0xae, 0x00, 0x10, 0x40, 0x81, 0xcf, 0xa1, 0xc8, 0xa6, 0xa8, 0x3f, 0xd3, 0x00, 0xd5, 0x80, 0xd9, 0xf1, 0xda, 0x12, 0xdb, 0x40, 0x20, 0x02, 0x8d, 0x14, 0xa4, 0xa6, 0xaf, };
+        uint8_t cmds[] = {0xae, 0x00, 0x10, 0x40, 0x81, 0xcf, 0xa1, 0xc8, 0xa6, 0xa8, 0x3f, 0xd3, 0x00, 0xd5, 0x80, 0xd9, 0xf1, 0xda, 0x12, 0xdb, 0x40, 0x20, 0x00, 0x8d, 0x14, 0xa4, 0xa6, 0xaf, };
         int i = 0;
         INT8U err1 = 0;
         
@@ -107,7 +107,8 @@ OLED_RES Fill_OLED_PAGE(int page, int seg_start, int seg_cnt, int pixel_appearan
 OLED_RES Flush_OLED_Mem(void)
  {
      int i;
-     uint8_t cmd[] = {0x00, 0x00, 0x00};
+     uint8_t cmd1[] = {0x21, 0, 127};
+     uint8_t cmd2[] = {0x22, 0, 7};
      
      if (DSPI_RES_OK != Open_DSPI_Dev(OLED_Dev_Data_1.DSPI_dev))
      {
@@ -115,27 +116,13 @@ OLED_RES Flush_OLED_Mem(void)
      }
      Set_DSPI_CTAR(OLED_Dev_Data_1.DSPI_dev, OLED_DSPI_CTAR_DBR, OLED_DSPI_CTAR_CPOL, OLED_DSPI_CTAR_CPHA, OLED_DSPI_CTAR_LSBFE, OLED_DSPI_CTAR_PCSSCK, OLED_DSPI_CTAR_PASC, OLED_DSPI_CTAR_PDT, OLED_DSPI_CTAR_PBR, OLED_DSPI_CTAR_CSSCK, OLED_DSPI_CTAR_ASC, OLED_DSPI_CTAR_DT, OLED_DSPI_CTAR_BR);
      Set_DSPI_PUSHR(OLED_Dev_Data_1.DSPI_dev, OLED_DSPI_PUSHR_CONT, OLED_DSPI_PUSHR_PCS);
-     OLED_PIN_DC = OLED_DC_DATA;
+     OLED_PIN_DC = OLED_DC_COMMAND;
+     DSPI_SYNC_Send_and_Receive_Data(OLED_Dev_Data_1.DSPI_dev, cmd1, NULL, sizeof(cmd1));
+     DSPI_SYNC_Send_and_Receive_Data(OLED_Dev_Data_1.DSPI_dev, cmd2, NULL, sizeof(cmd2));
+     OLED_PIN_DC = OLED_DC_COMMAND;
      for (i = 0; i < sizeof(OLED_Display_Memory_2); i += DSPI_ASYNC_SEND_DATA_MAX_LENGTH)
      {
-         DSPI_SYNC_Send_and_Receive_Data(OLED_Dev_Data_1.DSPI_dev, ((uint8_t *)OLED_Display_Memory_2 + i * DSPI_ASYNC_SEND_DATA_MAX_LENGTH), NULL, DSPI_ASYNC_SEND_DATA_MAX_LENGTH);
-     }
-     for (i = 0; i < OLED_PAGE_MAX; i++)
-     {
-         int j = 0;
-         uint8_t cmd[] = {0x00, 0x00, 0x00};
-
-         OLED_PIN_DC = OLED_DC_COMMAND;
-         cmd[0] = 0xB0 + (uint8_t)i;
-         cmd[1] = (((uint8_t)0 & 0xF0) >> 4) | 0x10;
-         cmd[2] = ( (uint8_t)0 & 0x0F);
-         OLED_PIN_DC = OLED_DC_COMMAND;
-         DSPI_SYNC_Send_and_Receive_Data(OLED_Dev_Data_1.DSPI_dev, cmd, NULL, sizeof(cmd));
-         OLED_PIN_DC = OLED_DC_DATA;
-         for (j = 0; j < OLED_SEG_MAX; j += DSPI_ASYNC_SEND_DATA_MAX_LENGTH)
-         {
-             DSPI_SYNC_Send_and_Receive_Data(OLED_Dev_Data_1.DSPI_dev, OLED_Display_Memory_2[i] + j, NULL, DSPI_ASYNC_SEND_DATA_MAX_LENGTH);
-         }
+         DSPI_SYNC_Send_and_Receive_Data(OLED_Dev_Data_1.DSPI_dev, (uint8_t *)OLED_Display_Memory_2 + i, NULL, DSPI_ASYNC_SEND_DATA_MAX_LENGTH);
      }
      Close_DSPI(OLED_Dev_Data_1.DSPI_dev);
      return OLED_RES_OK;
